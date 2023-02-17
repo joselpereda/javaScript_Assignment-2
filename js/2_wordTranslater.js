20de08e96d01684052fe3c66b58ff18e8e91c524
joselpereda <joselpereda@hotmail.com>
2023-02-16 22:27:41 -0800

    added code to 2_wordTranslater.js

---
+++ js/2_wordTranslater.js
@@ -0,0 +1,15 @@
+//Prompt user for language code
+let languageCode = prompt("Enter two-letter language code (es, de, en or fr):").toLowerCase();
+
+//Check code entered and display "hello world" in corresponding message
+if (languageCode === "es") {
+    document.write("Hola Mundo!");
+} else if (languageCode === "de") {
+    document.write("hallo Welt!");
+} else if (languageCode === "en") {
+    document.write("Hello World!");
+} else if (languageCode === "fr" ) {
+    document.write("Bonjour le monde!");
+} else {
+    document.write("Hello World!");
+}
